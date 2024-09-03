e4af34ef7d19e2536a691656a3175cda568f3a4d
Alanjunior45 <166314144+Alanjunior45@users.noreply.github.com>
2024-09-03 00:48:30 -0300

    feat: alterando routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Link,  } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 import AppRoutes from './Routes'
 import { Button } from 'primerreact/button'
 import './App.css'
 
 
 function App() {
-
   return (
-    <>
     <BrowserRouter>
-      <h1 className='lg:bg-[#0096D6] md:bg-orange-400 sm:bg-green-400 bg-slate-400 px-4 py-8 border-4 border-solid rounded border-black text-white'>Projeto Final 2024
+      <h1 className='lg:bg-[#0096D6] md:bg-orange-400 sm:bg-green-400 bg-slate-400 px-4 py-8 border-4 border-solid rounded border-black text-white'>
         <Link className="p-2 houver:underline" to="/">Home</Link>
-        <Link className="p-2 houver:uderline" to="/todepartamentos">Departamentos</Link>
+        <Link className="p-2 houver:uderline" to="/departamentos">Departamentos</Link>
       </h1>
+
       <div className="w-full max-w-[calc(100vw-16px)] md:max-w-[1280px] mx-auto mt-6">
-      <div className="grid grid-cols-12 gap-1">
-      <AppRoutes/>
-      </div>
-      </div>
-      </BrowserRouter>
-     </>
+        <div className="grid grid-cols-12 gap-1">
 
-
+          <AppRoutes/>
+        </div>
+      </div>
+    </BrowserRouter>
 
   )
 }
--- src/Pages/Departamentos/Listagem/index.tsx
+++ src/Pages/Departamentos/Listagem/index.tsx
@@ -2,6 +2,7 @@ import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 import { Button } from 'primereact/button'
 
+
 const Departamentos = () => {
 
   const departamentos = [
@@ -21,6 +22,20 @@ const Departamentos = () => {
         <>
         <Button icon="pi pi-pencil" rounded severity='warning' className='mr-2' />
         <Button icon="pi pi-trash" rounded severity='danger' />
+        <Button label="Success" severity="success" raised />
+        <Button label="Delete" icon="pi pi-trash" />
+        <Button icon="pi pi-check" aria-label="Filter" />
+<Button icon="pi pi-bookmark" severity="secondary" aria-label="Bookmark" />
+<Button icon="pi pi-search" severity="success" aria-label="Search" />
+<Button icon="pi pi-user" severity="info" aria-label="User" />
+<Button icon="pi pi-bell" severity="warning" aria-label="Notification" />
+<Button icon="pi pi-heart" severity="help" aria-label="Favorite" />
+<Button icon="pi pi-times" severity="danger" aria-label="Cancel" />
+
+
+
+
+
         </>
     )
   }
@@ -33,7 +48,7 @@ const Departamentos = () => {
       <div className="col-span-12">
         <DataTable value={departamentos}>
         <Column field="sigla" header="Sigla"/>
-        <Column field="Nome" header="Nome"/>
+        <Column field="nome" header="Nome"/>
         <Column header="Ação" body={bodyAcao} />
           
         </DataTable>
